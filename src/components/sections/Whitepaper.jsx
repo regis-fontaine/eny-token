83d688026d155a2e547dea5c24e68c81af8c01a8
regis-fontaine <regis.s.fontaine@gmail.com>
2021-07-08 00:14:21 +0200

    easy responsive on easy flex & first clean css

--- src/components/sections/Whitepaper.jsx
+++ src/components/sections/Whitepaper.jsx
@@ -10,7 +10,7 @@ const Whitepaper = () => {
             <section id={"whitepaper-eny"} >
                 <div className={"eny-title-container"} >
                     <h2>White Paper</h2>
-                    <p className={"eny-subtitle"} >WhitePaper.WhitePaper.WhitePaper.WhitePaper.WhitePaper.WhitePaper.WhitePaper</p>
+                    <p className={"eny-subtitle"} >WhitePaper.WhitePaper.WhitePaper.WhitePaper.WhitePaper</p>
                 </div>
                 <Button name="White Paper" href="#" />
             </section>
